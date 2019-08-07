890c84e7bd5419fb7d10e634cd18a95317a1d07b
Praveen Bisht <prvnbist@gmail.com>
2019-08-07 18:10:31 +0530

    Added: API changes from dev branch

--- filesystem.js
+++ filesystem.js
@@ -1,148 +1,150 @@
-const util = require('util');
+const util = require('util')
 
 // import {promisify} from 'util';
 // const {promisify} = require('util');
-const testFolder = 'mychatwidget';
-const fs = require('fs');
+const testFolder = 'mychatwidget'
+const fs = require('fs')
 // const readdir = util.promisify(fs.readdir);
-require("babel-polyfill");
+require('babel-polyfill')
 
 const readdir = util.promisify(fs.readdir, (resolve, reject) => {
-  resolve('ok');
-});
+	resolve('ok')
+})
 
 async function displayData(testFolder) {
-  try {
-    let data = await readdir(testFolder)//, (err, files) => {
-    let intermediateData = data.map(async (file) => {
-      let returnObject = {};
-      returnObject.value = file;
-      returnObject.id = testFolder + '/' + file;
-      let filenamearray = file.split('');
-      if (filenamearray[0] !== '.' && file !== 'node_modules') {
-        if (fs.lstatSync(testFolder + '/' + file).isDirectory()) {
-          let functionResponse = await displayData(testFolder + '/' + file);
-          returnObject.data = functionResponse;
-          returnObject.type = 'folder';
-          return returnObject;
-        } else {
-          returnObject.type = 'text';
-          return returnObject;
-        }
-      }
-      else
-        returnObject.type = 'text';
-      return returnObject;
-    });
-    return Promise.all(intermediateData).then(result => result)
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		let data = await readdir(testFolder) //, (err, files) => {
+		let intermediateData = data.map(async file => {
+			let returnObject = {}
+			returnObject.name = file
+			returnObject.path = testFolder + '/' + file
+			let filenamearray = file.split('')
+			if (filenamearray[0] !== '.' && file !== 'node_modules') {
+				if (fs.lstatSync(testFolder + '/' + file).isDirectory()) {
+					let functionResponse = await displayData(
+						testFolder + '/' + file
+					)
+					returnObject.children = functionResponse
+					returnObject.type = 'folder'
+					return returnObject
+				} else {
+					returnObject.type = 'file'
+					return returnObject
+				}
+			} else returnObject.type = 'file'
+			return returnObject
+		})
+		return Promise.all(intermediateData).then(result => result)
+	} catch (e) {
+		console.log(e)
+	}
 }
 
 async function displayFolders(testFolder) {
-  try {
-    let data = await readdir(testFolder)//, (err, files) => {
-    let intermediateData = data.forEach(async (file) => {
-      let returnObject = {};
-      let filenamearray = file.split('');
-      if (filenamearray[0] !== '.' && file !== 'node_modules') {
-        if (fs.lstatSync(testFolder + '/' + file).isDirectory()) {
-          let functionResponse = await displayFolders(testFolder + '/' + file);
-          returnObject.value = file;
-          returnObject.id = testFolder + '/' + file;
-          returnObject.data = functionResponse;
-          returnObject.type = 'folder';
-          // console.log(returnObject)
-          return returnObject;
-        }
-      }
-      // else
-      //   returnObject.type = 'text';
-      return returnObject;
-    });
-    return intermediateData
-    // console.log(intermediateData)
-    // return Promise.all(intermediateData).then(result => result)
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		let data = await readdir(testFolder) //, (err, files) => {
+		let intermediateData = data.forEach(async file => {
+			let returnObject = {}
+			let filenamearray = file.split('')
+			if (filenamearray[0] !== '.' && file !== 'node_modules') {
+				if (fs.lstatSync(testFolder + '/' + file).isDirectory()) {
+					let functionResponse = await displayFolders(
+						testFolder + '/' + file
+					)
+					returnObject.value = file
+					returnObject.id = testFolder + '/' + file
+					returnObject.data = functionResponse
+					returnObject.type = 'folder'
+					// console.log(returnObject)
+					return returnObject
+				}
+			}
+			// else
+			//   returnObject.type = 'text';
+			return returnObject
+		})
+		return intermediateData
+		// console.log(intermediateData)
+		// return Promise.all(intermediateData).then(result => result)
+	} catch (e) {
+		console.log(e)
+	}
 }
 
 function getFile(filePath) {
-  return new Promise((resolve, reject) => {
-    console.log(filePath)
-    let modifiedPath = '/' + filePath.split('./')[1];
-    let path = process.cwd();
-    try {
-      let buffer = fs.readFile(path + modifiedPath, (err, data) => {
-        if (err) {
-          resolve({ 'status': "fail" })
-        } else {
-          // console.log(data);
-          resolve(data.toString());
-        }
-      });
-    } catch (err) {
-      resolve({ 'status': 'fail' });
-    }
-  })
+	return new Promise((resolve, reject) => {
+		console.log(filePath)
+		let modifiedPath = '/' + filePath.split('./')[1]
+		let path = process.cwd()
+		try {
+			let buffer = fs.readFile(path + modifiedPath, (err, data) => {
+				if (err) {
+					resolve({ status: 'fail' })
+				} else {
+					// console.log(data);
+					resolve(data.toString())
+				}
+			})
+		} catch (err) {
+			resolve({ status: 'fail' })
+		}
+	})
 }
 
 function updateFile(body) {
-  return new Promise((resolve, reject) => {
-    let filePath = body.file;
-    let data = body.data;
-    console.log(body)
-    console.log(filePath)
-    let modifiedPath = '/' + filePath.split('./')[1];
-    var path = process.cwd();
-    console.log(modifiedPath);
-    console.log(path + modifiedPath)
-    fs.writeFileSync(path + modifiedPath, data, function (err) {
-      if (err) {
-        return console.log(err);
-      }
+	return new Promise((resolve, reject) => {
+		let filePath = body.file
+		let data = body.data
+		console.log(body)
+		console.log(filePath)
+		let modifiedPath = '/' + filePath.split('./')[1]
+		var path = process.cwd()
+		console.log(modifiedPath)
+		console.log(path + modifiedPath)
+		fs.writeFileSync(path + modifiedPath, data, function(err) {
+			if (err) {
+				return console.log(err)
+			}
 
-      console.log("The file was saved!");
-    });
-    resolve({ 'status': 'success' });
-  })
+			console.log('The file was saved!')
+		})
+		resolve({ status: 'success' })
+	})
 }
 
 function createNewFile(body) {
-  return new Promise((resolve, reject) => {
-    let filePath = body.file;
-    let objectType = body.objectType;
-    console.log(objectType)
-    console.log(filePath)
-    let newFilePath = '/' + filePath.split('./')[1];
-    var path = process.cwd(); // gives current working directory
-    console.log(newFilePath);
-    console.log(path + newFilePath);
+	return new Promise((resolve, reject) => {
+		let filePath = body.file
+		let objectType = body.objectType
+		console.log(objectType)
+		console.log(filePath)
+		let newFilePath = '/' + filePath.split('./')[1]
+		var path = process.cwd() // gives current working directory
+		console.log(newFilePath)
+		console.log(path + newFilePath)
 
-    let sourceFilePath = path + '/templates/' + objectType + '.json'
-    //    let extension;
-    //    if(objectType == 'recipes') {
-    //      extension = '.rec';
-    //    }
-    let destinationFilePath = path + newFilePath;// +extension;
-    fs.copyFile(sourceFilePath, destinationFilePath, (err) => {
-      if (err) {
-        reject({ 'status': 'file could not be created' })
-      } else {
-        //getFile(sourceFilePath)
-        // console.log('source.txt was copied to destination.txt');
-        resolve({ 'status': 'Success! file created' });
-      }
-    });
-  })
+		let sourceFilePath = path + '/templates/' + objectType + '.json'
+		//    let extension;
+		//    if(objectType == 'recipes') {
+		//      extension = '.rec';
+		//    }
+		let destinationFilePath = path + newFilePath // +extension;
+		fs.copyFile(sourceFilePath, destinationFilePath, err => {
+			if (err) {
+				reject({ status: 'file could not be created' })
+			} else {
+				//getFile(sourceFilePath)
+				// console.log('source.txt was copied to destination.txt');
+				resolve({ status: 'Success! file created' })
+			}
+		})
+	})
 }
 
 module.exports = {
-  getFile: getFile,
-  displayData: displayData,
-  updateFile: updateFile,
-  createNewFile: createNewFile,
-  displayFolders
+	getFile: getFile,
+	displayData: displayData,
+	updateFile: updateFile,
+	createNewFile: createNewFile,
+	displayFolders,
 }
