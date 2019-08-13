dc52f12e15ed206d321c1a0b1296cbecc109b8c7
Praveen Bisht <prvnbist@gmail.com>
2019-08-13 18:22:20 +0530

    Optimize: Used union for File/Folder and removed rest of the queries

--- src/schema/resolvers/index.js
+++ src/schema/resolvers/index.js
@@ -1,38 +1,17 @@
-const filesystem = require('../../filesystem')
 const folders = require('../../functions/folder')
 const files = require('../../functions/file')
 
-const allFiles = require('../../allFilesData.js')
-
 const resolvers = {
+	FolderOrFile: {
+		__resolveType(obj) {
+			if (obj.type === 'folder') return 'Folder'
+			if (obj.type === 'file') return 'File'
+		},
+	},
 	Query: {
-		content: async () => {
-			const data = await filesystem
-				.displayData('./filesystem')
-				.then(response => response)
-			const appendData = await {
-				name: 'Folder',
-				path: './filesystem',
-				type: 'folder',
-				children: data,
-			}
-			return appendData
-		},
 		folders: async () => {
-			const data = await filesystem
-				.displayFolders('./filesystem')
-				.then(response => response)
-			const appendData = await {
-				name: 'Folder',
-				path: './filesystem',
-				type: 'folder',
-				children: data,
-			}
-			return appendData
-		},
-		contentWithFilesData: async (_, args) => {
-			const data = await allFiles
-				.displayData(args.path)
+			const data = await folders
+				.getFolder('./filesystem')
 				.then(response => response)
 			const appendData = await {
 				name: 'Folder',
--- src/schema/typeDefs/index.js
+++ src/schema/typeDefs/index.js
@@ -1,38 +1,12 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-	"Schema for all files/folders"
-	type Content {
-		"Name of the folder/file"
-		name: String
-		"Relative path of the folder/file"
-		path: String
-		"Type of item, either folder or file"
-		type: String
-		"Size of the file, folder size not supported yet."
-		size: Int
-		"Get the file data"
-		data: String
-		"Array of nested folders/files"
-		children: [Content]
-	}
-	"Schema for only fetching list of folders"
+	union FolderOrFile = Folder | File
 	type Folder {
-		"Name of the folder"
-		name: String
-		"Relative path of the folder"
-		path: String
-		"Type of item"
-		type: String
-		"Return list of nested folders"
-		children: [Folder]
-	}
-	type ContentWithFilesData {
 		name: String
 		path: String
 		type: String
-		content: String
-		children: [ContentWithFilesData]
+		children: [FolderOrFile]
 	}
 	type File {
 		name: String
@@ -44,11 +18,7 @@ const typeDefs = gql`
 		createdAt: String
 	}
 	type Query {
-		"Query to fetch all the nested folders/files"
-		content: Content
-		"Query to fetch all the nested folders"
 		folders: Folder
-		contentWithFilesData(path: String): ContentWithFilesData
 		getFile(path: String!): File
 	}
 `
