361cd09210c81f1331529690e3b60297686a6091
Praveen Bisht <prvnbist@gmail.com>
2019-08-16 12:59:23 +0530

    Added: Helper Function - getFolderSize

---
+++ src/utils/getFolderSize.js
@@ -0,0 +1,18 @@
+const _ = require('lodash')
+const fs = require('fs')
+
+const getFolderSize = dir => {
+	dir = dir.replace(/\/$/, '')
+	return _.flatten(
+		fs.readdirSync(dir).map(function(file) {
+			var fileOrDir = fs.statSync([dir, file].join('/'))
+			if (fileOrDir.isFile()) {
+				return (dir + '/' + file).replace(/^\.\/\/?/, '')
+			} else if (fileOrDir.isDirectory()) {
+				return getFolderSize([dir, file].join('/'))
+			}
+		})
+	)
+}
+
+module.exports = getFolderSize
