cdc731e8f4d0c0bc2428ab3fd54538d307be15fc
Praveen Bisht <prvnbist@gmail.com>
2019-08-17 15:30:37 +0530

    Fixed: Path forward/backward slash incompatibility across diff OS

--- src/functions/file.js
+++ src/functions/file.js
@@ -2,16 +2,18 @@ const fs = require('fs')
 const path = require('path')
 
 const createFile = (givenPath, givenType) => {
-	try {
-		let newFilePath = '/' + givenPath.split('./')[1]
-		var fullPath = process.cwd()
-		let sourceFilePath = `${fullPath}\\src\\templates\\${givenType}.json`
-		let destinationFilePath = fullPath + newFilePath.split('/').join('\\')
-		fs.copyFileSync(sourceFilePath, destinationFilePath)
-		return 'File created successfully!'
-	} catch (err) {
-		return new Error(err)
-	}
+	return new Promise((resolve, reject) => {
+		let source = `./src/templates/${givenType}.json`
+		let destination = `./${givenPath.split('./')[1]}`
+		if (fs.existsSync(source)) {
+			fs.copyFile(source, destination, err => {
+				if (err) reject(new Error('File could not be created!'))
+				resolve('File created successfully!')
+			})
+		} else {
+			reject(new Error(`Template file ${givenType} doesn't exists!`))
+		}
+	})
 }
 
 const deleteFile = givenPath => {
