e9ddf1728d925444f7a25fcc89fdc8a90302cf1d
Praveen Bisht <prvnbist@gmail.com>
2019-12-05 16:34:52 +0530

    feat(git): added addAndCommit & removeAndCommit

--- src/functions/git.js
+++ src/functions/git.js
@@ -6,76 +6,42 @@ git.plugins.set('fs', fs)
 
 const { repoDir, getRelFilePath } = require('../utils/parsePath')
 
-const stageChanges = (type, dir, filepath) => {
+const addAndCommit = (filePath, author, committer, message) => {
 	return new Promise((resolve, reject) => {
-		if (type === 'add') {
+		if (fs.existsSync(filePath)) {
 			git.add({
-				dir,
-				filepath,
-			}).catch(error => reject(new Error(error)))
-			return resolve(1)
-		} else if (type === 'remove') {
-			git.remove({
-				dir,
-				filepath,
-			}).catch(error => reject(new Error(error)))
-			return resolve(1)
+				dir: repoDir(filePath),
+				filepath: getRelFilePath(filePath),
+			})
+			const sha = git.commit({
+				dir: repoDir(filePath),
+				author,
+				committer,
+				message,
+			})
+			return resolve(sha)
 		}
+		return reject(`File: ${path.basename(filePath)} doesn't exist!`)
 	})
 }
 
-const addAndCommit = async (givenPath, author, committer) => {
-	try {
-		await git.add({
-			dir: repoDir(givenPath),
-			filepath: getRelFilePath(givenPath),
+const removeAndCommit = (filePath, author, committer) => {
+	return new Promise(resolve => {
+		git.remove({
+			dir: repoDir(filePath),
+			filepath: getRelFilePath(filePath),
 		})
 		const sha = git.commit({
-			dir: repoDir(givenPath),
+			dir: repoDir(filePath),
 			author,
 			committer,
-			message: `Added: ${path.basename(givenPath)}`,
+			message: `Deleted: ${path.basename(filePath)}`,
 		})
-
-		return sha
-	} catch (error) {
-		throw error
-	}
-}
-
-const removeAndCommit = async (givenPath, author, committer) => {
-	try {
-		await git.remove({
-			dir: repoDir(givenPath),
-			filepath: getRelFilePath(givenPath),
-		})
-		const sha = git.commit({
-			dir: repoDir(givenPath),
-			author,
-			committer,
-			message: `Deleted: ${path.basename(givenPath)}`,
-		})
-
-		return sha
-	} catch (error) {
-		throw error
-	}
-}
-
-const gitCommit = (givenPath, author, committer, message) => {
-	return git
-		.commit({
-			dir: repoDir(givenPath),
-			author: author,
-			commiter: committer,
-			message: message,
-		})
-		.then(sha => sha)
+		return resolve(sha)
+	})
 }
 
 module.exports = {
-	stageChanges,
-	gitCommit,
 	addAndCommit,
 	removeAndCommit,
 }
