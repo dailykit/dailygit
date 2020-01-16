07c7c25b7126fe0cd5d415c3d56093256401ff1f
Praveen Bisht <prvnbist@gmail.com>
2020-01-16 16:00:14 +0530

    cleanup: removed unused helpers

--- src/utils/parsePath.js
+++ src/utils/parsePath.js
@@ -1,33 +1,15 @@
-const baseFolder = './../apps/'
-
-const getRepoPath = givenPath =>
-    givenPath
-        .split(baseFolder)
-        .filter(Boolean)[0]
-        .split('/')
-        .slice(0, 3)
-        .join('/')
-
-const getRelFilePath = givenPath =>
-    givenPath
-        .split(baseFolder)
-        .filter(Boolean)[0]
-        .split('/')
-        .slice(3)
-        .join('/')
-
-const repoDir = givenPath => `${baseFolder}${getRepoPath(givenPath)}`
-const getAppName = givenPath => getRepoPath(givenPath).split('/')[0]
-const getRepoName = givenPath =>
-    getRepoPath(givenPath)
-        .split('/')
-        .pop()
+const getRepoPath = path =>
+   `${path
+      .split('/')
+      .slice(0, 3)
+      .join('/')}`
+const getFilePath = path =>
+   `${path
+      .split('/')
+      .slice(3)
+      .join('/')}`
 
 module.exports = {
-    getRelFilePath,
-    getRepoPath,
-    repoDir,
-    getAppName,
-    getRepoName,
-    baseFolder,
+   getRepoPath,
+   getFilePath,
 }
